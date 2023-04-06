84f06435f82ee15e9731ef35fa5ffc1bfd59e269
phinqevol <phinqevol@gmail.com>
2023-04-06 17:16:43 +0700

    init commit

---
+++ EraShops.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EraShops.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
